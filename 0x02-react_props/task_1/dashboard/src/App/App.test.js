cfababecbed2087421ab5ab6894428c9c007b14f
calebdfk <dkweteza@outlook.com>
2024-07-30 20:31:15 +0200

    update test

--- 0x02-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
 
-const wrapper = shallow(<App />);
+describe('App Component', () => {
+  it('contains the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications).length).toBe(1);
+  });
 
-it('renders without crashing', () => {
-  shallow(<App />);
+  it('contains the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+
+  it('contains the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login).length).toBe(1);
+  });
 });

f1a0c2a1619661b323e284b4489ecab834c95a90
calebdfk <dkweteza@outlook.com>
2024-08-06 23:09:32 +0200

    Lifestyle update

--- 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function NotificationItem({ type = 'default', html = {}, value = '' }) {
-  let li;
-
+export default function NotificationItem({ type, value, html }) {
   if (value) {
-    li = <li data-notification-type={type}>{value}</li>;
+    return (<li data-notification-type={type}>{value}</li>);
   } else {
-    li = <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>;
+    return (
+      <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+    );
   }
-
-  return li;
 }
+
+NotificationItem.defaultProps = {
+  type: "default",
+  value: "",
+  html: {},
+};
 
 NotificationItem.propTypes = {
   type: PropTypes.string,
@@ -20,5 +24,3 @@ NotificationItem.propTypes = {
     __html: PropTypes.string,
   }),
 };
-
-export default NotificationItem;

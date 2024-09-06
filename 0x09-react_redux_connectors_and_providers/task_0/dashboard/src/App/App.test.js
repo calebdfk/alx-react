f1306207aa6c7d45e6b182efdd71e6cadc976e1b
calebdfk <dkweteza@outlook.com>
2024-09-07 00:07:46 +0200

    Update mapStateToProps

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,20 +1,21 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import { StyleSheetTestUtils } from 'aphrodite';
-import uiReducer, { initialState } from "../reducers/uiReducer";
+import { shallow, mount } from "enzyme";
+import React from "react";
+import App, { listNotificationsInitialState, mapStateToProps } from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+import AppContext, { user, logOut } from "./AppContext";
+
+import { fromJS } from "immutable";
 import { createStore } from "redux";
-import { mapStateToProps } from './App';
 import { Provider } from "react-redux";
+import uiReducer, { initialState } from "../reducers/uiReducer";
 
 const store = createStore(uiReducer, initialState);
 
-describe('redux related tests', () => {
-  it('testing mapStateToProps', () => {
-       const state = {
-      uiReducer: {
-        isUserLoggedIn: true,
-      },
-    };
+describe("<App />", () => {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      isUserLoggedIn: true,
+    });
 
     const result = mapStateToProps(state);
 
